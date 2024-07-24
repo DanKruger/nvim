0eef8ef8c3e7731f01d838d3981f786feee5a49f
Daniel Kruger <danielkruger98@gmail.com>
2024-07-24 11:41:37 +0200

    use variable config

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -1,39 +1,57 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set scrolloff=10")
+vim.cmd("let g:netrw_liststyle = 3")
+vim.g.mapleader = " " -- Set the leader key
 
-vim.g.mapleader = " "                      -- Set the leader key
-vim.keymap.set("n", "<leader>w", ":w<CR>") -- Write File (Save)
-vim.opt.updatetime = 100                   -- Set buffer update time
+local option = vim.opt
+local keybind = vim.keymap
+
+-- Indent configuration
+option.tabstop = 4    -- Tab size
+option.shiftwidth = 4 -- indent width
+option.scrolloff = 10
+option.softtabstop = 4
+option.expandtab = true  -- Expand tab to spaces
+option.autoindent = true -- copy indent of current line to new line
+
+option.cursorline = true -- Highlight line cursor is on
+option.updatetime = 100  -- Set buffer update time
+
+-- Search settings
+option.ignorecase = true -- Ignore case while searching
+option.smartcase = true  -- if includes mixed case, assume you mean case sensitive
+
+option.termguicolors = true
+option.background = "dark" -- set background to dark version of theme
+
+-- Clipboard configuration
+option.clipboard:append("unnamedplus")
+
+keybind.set("n", "<leader>w", ":w<CR>") -- Write File (Save)
 
 -- Line number configuration
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.signcolumn = "auto:9"
+option.number = true
+option.relativenumber = true
+option.signcolumn = "auto:9"
 
---[[ vim.opt.statuscolumn = '%#NonText# %{&nu?v:lnum:""} %=' ]]
+--[[ option.statuscolumn = '%#NonText# %{&nu?v:lnum:""} %=' ]]
 
 -- Tab navigation
-vim.keymap.set("n", "<C-j>", ":tabp<CR>") -- Switch to tab on left
-vim.keymap.set("n", "<C-k>", ":tabn<CR>") -- Switch to tab on right
-vim.keymap.set("n", "<C-T>", ":tabe<CR>") -- Create new tab
-vim.keymap.set("n", "<C-x>", ":tabc<CR>") -- Close current tab
+keybind.set("n", "<A-j>", ":tabp<CR>") -- Switch to tab on left
+keybind.set("n", "<A-k>", ":tabn<CR>") -- Switch to tab on right
+keybind.set("n", "<A-t>", ":tabe<CR>") -- Create new tab
+keybind.set("n", "<A-x>", ":tabc<CR>") -- Close current tab
 
 -- Window management
-vim.keymap.set("n", "<leader>k", "<C-w>s") -- Create new window on the side
-vim.keymap.set("n", "<leader>j", "<C-w>v") -- Create new window below
-vim.keymap.set("n", "<leader>q", "<C-w>q") -- Close window (same as :q)
+option.splitright = true                -- Windows will split to the right
+option.splitbelow = true                -- Windows will split below
+keybind.set("n", "<leader>k", "<C-w>s") -- Create new window on the side
+keybind.set("n", "<leader>j", "<C-w>v") -- Create new window below
+keybind.set("n", "<leader>q", "<C-w>q") -- Close window (same as :q)
 
 -- Window resizing
-vim.keymap.set("n", "<A-Right>", ":vert res +5<CR>") -- horizontally by 5
-vim.keymap.set("n", "<A-Left>", ":vert res -5<CR>")  -- horizontally by -5
-vim.keymap.set("n", "<A-Up>", ":res +5<CR>")         -- vertically by 5
-vim.keymap.set("n", "<A-Down>", ":res -5<CR>")       -- vertically by -5
+keybind.set("n", "<A-Right>", ":vert res +5<CR>") -- horizontally by 5
+keybind.set("n", "<A-Left>", ":vert res -5<CR>")  -- horizontally by -5
+keybind.set("n", "<A-Up>", ":res +5<CR>")         -- vertically by 5
+keybind.set("n", "<A-Down>", ":res -5<CR>")       -- vertically by -5
 
--- Window navigation
-vim.keymap.set("n", "<A-l>", "<C-w>l") -- Switch to window on the right
-vim.keymap.set("n", "<A-h>", "<C-w>h") -- Switch to window on the left
-vim.keymap.set("n", "<A-k>", "<C-w>k") -- Switch to window above
-vim.keymap.set("n", "<A-j>", "<C-w>j") -- Switch to window below
+-- Clear search highlights
+keybind.set("n", "<leader>h", ":nohl<CR>", { desc = "Clear search highlights" })
