fc2f0863b93f9c836d42fe26a77ac97db61dab16
Daniel Kruger <danielkruger98@gmail.com>
2024-07-24 20:16:58 +0200

    Update treesitter
    Now loads on new buffers only

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,19 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-
-		-- this is where you add language support for highlighting and syntax
-		config.setup({
-			ensure_installed = { "javascript", "html", "css", "python", "lua", "xml", "json" },
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
+    config = function()
+        local treesitter = require("nvim-treesitter.configs")
+        -- this is where you add language support for highlighting and syntax
+        treesitter.setup({
+            ensure_installed = { "java", "javascript", "html", "css", "python", "lua", "xml", "json" },
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+            autotag = { enable = true },
+        })
+    end,
 }
