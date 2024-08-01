64c5a8305a6ce849ea5a2d60d02f4b805fe9e3e9
Daniel Kruger <danielkruger98@gmail.com>
2024-08-01 07:59:37 +0200

    remove gutter icons from neotree conf

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -13,15 +13,12 @@ return {
 		{ "<C-e>", "<cmd>Neotree filesystem reveal left<CR>", desc = "Open filesystem to the left" },
 	},
 	config = function()
-		vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-		vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 		require("neo-tree").setup({
 			close_if_last_window = true,
 			filesystem = {
 				always_show = {
 					".gitignore",
+					".gitlab-ci.yml",
 				},
 			},
 			follow_current_file = {
