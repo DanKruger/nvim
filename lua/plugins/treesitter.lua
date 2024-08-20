8af6cca31764bcad5fd1554fa3b963c624551b7e
Daniel Kruger <danielkruger98@gmail.com>
2024-08-20 12:06:18 +0200

    Update treesitter
    add markdown language to ensure installed
    add other config options

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,11 +9,21 @@ return {
         local treesitter = require("nvim-treesitter.configs")
         -- this is where you add language support for highlighting and syntax
         treesitter.setup({
-            ensure_installed = { "java", "javascript", "html", "css", "python", "lua", "xml", "json" },
+            ensure_installed = { "java", "javascript", "html", "css", "markdown", "python", "lua", "xml", "json" },
+            modules = {},
             auto_install = true,
+            sync_install = true,
+            ignore_install = {},
             highlight = { enable = true },
             indent = { enable = true },
             autotag = { enable = true },
+            disable = function(lang, buf)
+                local max_filesize = 100 * 1024 -- 100 KB
+                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                if ok and stats and stats.size > max_filesize then
+                    return true
+                end
+            end,
         })
     end,
 }
