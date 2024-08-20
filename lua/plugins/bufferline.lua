502c2271d215d684261d58f201814e2182b49257
Daniel Kruger <danielkruger98@gmail.com>
2024-08-20 12:10:40 +0200

    Extend bufferline config

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,10 +2,29 @@ return {
 	"akinsho/bufferline.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	version = "*",
-	opts = {
-		options = {
-			mode = "tabs",
-			seperator_style = "slant",
-		},
-	},
+	config = function()
+		require("bufferline").setup({
+			options = {
+				mode = "tabs",
+				seperator_style = "slope",
+				indicator = {
+					style = "none",
+				},
+				auto_toggle_bufferline = true,
+				diagnostics = "nvim_lsp",
+				diagnostics_indicator = function(count, level)
+					local icon = level:match("error") and " " or " "
+					return " " .. icon .. count
+				end,
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "File Explorer",
+						highlight = "Directory",
+						seperator = true,
+					},
+				},
+			},
+		})
+	end,
 }
