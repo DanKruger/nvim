a5b945c45277cb86e1622652ebc4d551f5b60b67
Daniel Kruger <danielkruger98@gmail.com>
2024-08-20 12:55:54 +0200

    update keybinds

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -1,9 +1,13 @@
 return {
 	{
 		"vim-test/vim-test",
-		config = function()
-			vim.keymap.set("n", "<leader>n", "<cmd>TestNearest<CR>")
-			vim.keymap.set("n", "<leader>m", "<cmd>TestFile<CR>")
-		end,
+		-- config = function()
+		--     vim.keymap.set("n", "<leader>tt", "<cmd>TestNearest<CR>")
+		--     vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<CR>")
+		-- end,
+		keys = {
+			{ "<leader>tt", "<cmd>TestNearest<CR>", desc = "Test Nearest Test" },
+			{ "<leader>tf", "<cmd>TestFile<CR>", desc = "Test Current File" },
+		},
 	},
 }
