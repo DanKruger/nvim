2eb2471ae7d69e7d7e447eaffe901442e8de8b93
Daniel Kruger <danielkruger98@gmail.com>
2024-08-20 12:09:27 +0200

    to cmd

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -2,8 +2,8 @@ return {
 	{
 		"vim-test/vim-test",
 		config = function()
-			vim.keymap.set("n", "<leader>n", ":TestNearest<CR>")
-			vim.keymap.set("n", "<leader>m", ":TestFile<CR>")
+			vim.keymap.set("n", "<leader>n", "<cmd>TestNearest<CR>")
+			vim.keymap.set("n", "<leader>m", "<cmd>TestFile<CR>")
 		end,
 	},
 }
