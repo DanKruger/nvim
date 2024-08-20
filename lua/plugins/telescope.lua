0a01f3ae481e617c3c8f65475097a19e2d193ccc
Daniel Kruger <danielkruger98@gmail.com>
2024-08-20 12:08:39 +0200

    Update telescope config
    move telescope config to main telescope table
    move ui select config to telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,19 +4,17 @@ return {
 		tag = "0.1.5",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+			"sharkdp/fd",
 		},
 		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-			vim.keymap.set("n", "<C-g>", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-			vim.keymap.set("n", "gr", builtin.lsp_references, {})
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
 			require("telescope").setup({
+				defaults = {
+					layout_strategy = "vertical",
+					layout_config = {
+						vertical = { width = 0.7 },
+					},
+				},
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({}),
@@ -24,6 +22,12 @@ return {
 				},
 			})
 			require("telescope").load_extension("ui-select")
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+			vim.keymap.set("n", "<C-g>", builtin.git_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fs", builtin.grep_string, {})
+			vim.keymap.set("n", "gr", builtin.lsp_references, {})
 		end,
 	},
 }
