731f7a77eb4d5cf1d5b8aa9904a636e7811ac438
Daniel Kruger <danielkruger98@gmail.com>
2024-08-20 12:05:15 +0200

    Add noice plugin
    Makes everything look pretty

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -4,8 +4,12 @@ return {
     priority = 1000,
     config = function()
         require("kanagawa").setup({
+            transparent = true,
+            styles = {
+                sidebars = "transparent",
+                floats = "transparent",
+            },
             theme = "wave",
-            -- theme = "wave",
             colors = {
                 theme = {
                     all = {
@@ -15,6 +19,23 @@ return {
                     },
                 },
             },
+
+            overrides = function(colors)
+                local theme = colors.theme
+                return {
+                    NormalFloat = { bg = "none" },
+                    FloatBorder = { bg = "none" },
+                    FloatTitle = { bg = "none" },
+
+                    TelescopeTitle = { fg = theme.ui.special, bold = true },
+                    TelescopePromptNormal = { bg = "none" },
+                    TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = "none" },
+                    TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = "none" },
+                    TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = "none" },
+                    TelescopePreviewNormal = { bg = "none" },
+                    TelescopePreviewBorder = { bg = "none", fg = theme.ui.bg_dim },
+                }
+            end,
         })
         vim.cmd.colorscheme("kanagawa-wave")
     end,
