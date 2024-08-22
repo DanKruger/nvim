5bd35951aaeab34a7ee16261f2b11f6b736feb4b
Daniel Kruger <danielkruger98@gmail.com>
2024-08-22 14:18:44 +0200

    try to get stylua to use spaces

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,10 +5,14 @@ return {
         local null_ls = require("null-ls")
         local sources = {
             -- Formatters
-            null_ls.builtins.formatting.stylua, -- This is for LUA, which the config is written in
+            null_ls.builtins.formatting.stylua.with({
+                extra_args = { { "indent_type", "spaces" }, { "collapse_simple_statement", "always" } }, -- This is for LUA, which the config is written in
+            }),
             null_ls.builtins.formatting.black,
             null_ls.builtins.formatting.prettier.with({ extra_args = { "--tab-width", 4 } }),
             null_ls.builtins.formatting.shfmt,
+
+            null_ls.builtins.completion.tags,
 
             -- Linters
             -- null_ls.builtins.diagnostics.checkstyle.with({ extra_args = { "-c", "/sun_checks.xml" } }),
@@ -17,6 +21,6 @@ return {
 
         -- this is the keybind for formatting
         vim.keymap.set("n", "<leader>g", vim.lsp.buf.format, {})
-        vim.keymap.set("n", "<leader>fi", "gg=G<CR>", {}) -- Indent whole file
+        vim.keymap.set("n", "<leader>fi", "gg=G<C-o>", {}) -- Indent whole file
     end,
 }
